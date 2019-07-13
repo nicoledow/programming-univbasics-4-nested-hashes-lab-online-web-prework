bffe23735b3fc0c4c2fc0c03c6f2b0228fbf9f9f
Nicole Dow <nikki.dow71@gmail.com>
2019-07-13 00:55:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -120,6 +120,20 @@ def changing_alan
 end
 
 def adding_to_dennis
-
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
 
 end
